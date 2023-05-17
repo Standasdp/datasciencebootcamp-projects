4d2320462d8591f471723e21ad344c18833d6ccd
Warangkhana Duangpaen <116612718+Standasdp@users.noreply.github.com>
2023-05-17 22:43:31 +0700

    Delete ML-101.R

--- R/Machine Learning/ML-101.R
+++
@@ -1,8 +0,0 @@
-#Start Learning with Bootcamp7 in Data Transformation
-#rm(list=ls())
-#print("Hello wolrd ")
-#print('hello')
-
-## SetWorkDirectory
-#setwd("C:/Users/User/Desktop/RBootcamp/ML")
-#getwd()
